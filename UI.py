2dd26a57aaac464a56710fff4aeb439a01dad066
KornienkoDV <WorkStation@roskazna.ru>
2024-05-08 13:53:57 +0300

    Creating the delete function. Added comments for ease of use

--- UI.py
+++ UI.py
@@ -7,37 +7,31 @@ def interface():
     while StartProg:
         print("Производится работа с таблицами. Введите команду:\n 1-Вывод таблицы\n 2-Добавление новых значений\n 3-Изменение содержимого строки по коду\n 4-Удаление строки\n 5-Сохранить изменения в фаил\n 6-Завершение работы")
         command = int(input('Введите число - '))
+
+        #Проверка введеной команды
         while command > 6 or command < 1:
             print("Неправильный ввод")
             command = int(input('Введите число - '))
 
+        #Список команд
         match command:
-            case 1: print(UD.OutputData())
-            case 2: InputData()
-            case 3: DataChange()
-            case 4:
-                pass
-            case 5: UD.SaveData()
-            case _:
-                StartProg = False
-
-def InputData():
-    UD.InputData(InputCheck())
-
-
-def DataChange():
-    DataRowID = 1
-    RowMax = UD.OutputData().shape[0]
-    CheckWhileTrue = True
-    while CheckWhileTrue:
-        DataRowID = int(input("Введите код строки которую необходимо изменить - "))
-        if DataRowID > RowMax or DataRowID < 0:
-            print("Неправильный ввод")
-        else:
-            CheckWhileTrue = False
-
-    UD.RowEdit(DataRowID,InputCheck())
-    
+            case 1: print(UD.OutputData()) #Показ таблицы
+            case 2: UD.InputData(InputCheck()) #Добавление в таблицу
+            case 3: 
+                IdRow = InputCheckId()
+                print(f"Производится изменение строки:")
+                print(UD.OutputData("Row",IdRow))
+                UD.RowEdit(IdRow,InputCheck()) #Изменение в таблице
+            case 4: 
+                IdRow = InputCheckId()
+                print(f"Производится удаление строки:")
+                print(UD.OutputData("Row",IdRow))
+                UD.DeleteRow(IdRow) #Удаление из таблицы
+                print(f"Строка {IdRow} удалена, произведён перерасчет кода строк")
+            case 5: 
+                UD.SaveData() # Сохранение изменений в файл
+                print(f"Произведено сохранение в фаил")
+            case _: StartProg = False # Завершение работы программы
     
 def InputCheck():
     DataRow = list()
@@ -53,4 +47,15 @@ def InputCheck():
             CheckWhileTrue = False
             print(f"Сохранены следующие изменения:\n{HeadList}\n{DataRow}")
             return DataRow
-                
+
+def InputCheckId():
+    DataRowID = 1
+    RowMax = UD.OutputData().shape[0]
+    CheckWhileTrue = True
+    while CheckWhileTrue:
+        DataRowID = int(input("Введите код строки - "))
+        if DataRowID > RowMax or DataRowID < 0:
+            print("Неправильный ввод")
+        else:
+            CheckWhileTrue = False
+            return DataRowID
--- logger.py
+++ logger.py
@@ -8,14 +8,17 @@ class UserData:
     #Блок вывода таблицы или её частей
     def OutputData(self, OutputMethod = "Full",RowIndex = 0):
         if OutputMethod == "Full": 
-            return self.DataCSV.head()
+            return self.DataCSV.head(100)
         elif OutputMethod == "Head":
             return self.DataCSV.columns.tolist()
         elif OutputMethod == "Row":
             return self.DataCSV.loc[RowIndex]
             
     
-    #Блок сохранение\изменение данных
+    #Блок сохранение\изменение\удаление данных
     def InputData(self,DataToSave): self.DataCSV.loc[ len(self.DataCSV.index )] = DataToSave
     def RowEdit(self,RowIndex,RowList): self.DataCSV.loc[RowIndex] = RowList
+    def DeleteRow(self,RowIndex): 
+        self.DataCSV = self.DataCSV.drop(RowIndex)
+        self.DataCSV = self.DataCSV.reset_index(drop=True)
     def SaveData(self): self.DataCSV.to_csv("DataUser.csv",index=False)
